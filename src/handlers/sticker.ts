7145e0628830d4a7db52abe059c3f3398514158c
tfkhdyt <tfkhdyt@pm.me>
2022-09-28 10:58:08 +0700

    refactor(sticker): refactor sticker handler

--- src/handlers/sticker.ts
+++ src/handlers/sticker.ts
@@ -2,39 +2,34 @@ import { Message } from 'whatsapp-web.js';
 
 import goErrorHandler from '../utils/goErrHandler';
 import printLog from '../utils/logger';
-import parseOptions from '../utils/parseOptions';
 import { client } from './../index';
 
 type StickerHandlerParams = {
-  options: string[];
-  command: string[];
   message: Message;
   phoneNumber: string;
+  stickerName: string | null;
+  stickerAuthor: string | null;
 };
 
 const stickerHandler = async ({
-  options,
-  command,
   message,
   phoneNumber,
+  stickerName,
+  stickerAuthor,
 }: StickerHandlerParams) => {
-  const { stickerName, stickerAuthor } = parseOptions(options);
-
-  if (command[0] === '!sticker' && message.type === 'image') {
-    const [media, err] = await goErrorHandler(() => message.downloadMedia());
-    if (err instanceof Error || !media) {
-      message.reply('Terjadi kesalahan pada saat mendownload gambar');
-      return console.error('Error when downloading media |', err);
-    }
-
-    client.sendMessage(message.from, media, {
-      sendMediaAsSticker: true,
-      stickerName: stickerName ?? 'tfkhdyt sticker',
-      stickerAuthor: stickerAuthor ?? 'tfkhdyt',
-    });
-
-    printLog(phoneNumber, stickerName, stickerAuthor);
+  const [media, err] = await goErrorHandler(() => message.downloadMedia());
+  if (err instanceof Error || !media) {
+    message.reply('Terjadi kesalahan pada saat mendownload gambar');
+    return console.error('Error when downloading media |', err);
   }
+
+  await client.sendMessage(message.from, media, {
+    sendMediaAsSticker: true,
+    stickerName: stickerName ?? 'tfkhdyt sticker',
+    stickerAuthor: stickerAuthor ?? 'tfkhdyt',
+  });
+
+  printLog(phoneNumber, stickerName, stickerAuthor);
 };
 
 export default stickerHandler;
--- src/listeners/message.ts
+++ src/listeners/message.ts
@@ -2,30 +2,39 @@ import { Message } from 'whatsapp-web.js';
 
 import stickerHandler from '../handlers/sticker';
 import goErrorHandler from '../utils/goErrHandler';
+import parseOptions from '../utils/parseOptions';
+import { client } from './../index';
 
 const messageListener = async (message: Message) => {
   // stop the listener if message is from a status or from a group
   if (message.isStatus || message.author) return;
 
   // get contact info
-  // const contact = await message.getContact();
   const [contact, err] = await goErrorHandler(() => message.getContact());
   if (err instanceof Error || !contact) {
     message.reply('Terjadi kesalahan pada saat mendapatkan info kontak');
     return console.error('Error when getting contact |', err);
   }
 
-  // get command
   const command = message.body.split('--').map((cmd) => cmd.trim());
   const options = command.slice(1).join(' ').split('" ');
+  const { stickerName, stickerAuthor } = parseOptions(options);
 
   // handle sticker
-  stickerHandler({
-    command,
-    message,
-    options,
-    phoneNumber: contact.id.user,
-  });
+  if (command[0] === '!sticker' && message.type === 'image') {
+    await stickerHandler({
+      message,
+      phoneNumber: contact.id.user,
+      stickerName,
+      stickerAuthor,
+    });
+
+    if (!contact.name || !contact.name.endsWith('(DONATUR)'))
+      client.sendMessage(
+        message.from,
+        'Merasa terbantu oleh bot ini? Ingin mendukung saya di project selanjutnya? Anda bisa bantu saya dengan donasi melalui link berikut ini\n\nhttps://saweria.co/tfkhdyt\n\nSetelah Anda melakukan donasi, pesan ini akan hilang di-request selanjutnya.'
+      );
+  }
 };
 
 export default messageListener;
