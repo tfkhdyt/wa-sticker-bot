9aa5a75b8de96c41bb00c7ece1027f10bd8861fc
tfkhdyt <tfkhdyt@pm.me>
2022-09-29 07:21:29 +0700

    fix(help handler): remove --

--- src/handlers/help.ts
+++ src/handlers/help.ts
@@ -9,13 +9,13 @@ Kirim gambar dengan deskripsi sebagai berikut:
 \`\`\`!sticker\`\`\`
 
 *Buat stiker dengan custom name*
-\`\`\`!sticker -- name="Nama stiker"\`\`\`
+\`\`\`!sticker name="Nama stiker"\`\`\`
 
 *Buat stiker dengan custom author*
-\`\`\`!sticker -- author="Nama author"\`\`\`
+\`\`\`!sticker author="Nama author"\`\`\`
 
 *Buat stiker dengan custom name dan author*
-\`\`\`!sticker -- name="Nama stiker" author="Nama author"\`\`\`
+\`\`\`!sticker name="Nama stiker" author="Nama author"\`\`\`
 
 *Tampilkan user guide*
 \`\`\`!help\`\`\`
